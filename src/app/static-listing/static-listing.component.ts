87eba02173a18a4599297d1c38eee90d1b647b48
Rachel <solcode@outlook.com>
2019-08-14 16:58:46 -0400

    Added in MDC component dependencies. --Added button ripples --Changed static listing to include fieldset and legend. --Adjusted color palette.

--- src/app/static-listing/static-listing.component.ts
+++ src/app/static-listing/static-listing.component.ts
@@ -1,17 +1,29 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChildren, QueryList } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { MDCRipple } from '@material/ripple';
 
 @Component({
   selector: 'app-static-listing',
   templateUrl: './static-listing.component.html',
   styleUrls: ['./static-listing.component.scss']
 })
-export class StaticListingComponent {
+export class StaticListingComponent implements AfterViewInit {
   staticListing;
+  ripple: MDCRipple[];
+  @ViewChildren('rippleComponent') childComponents: QueryList<any>;
 
   constructor(
-    private http: HttpClient
+    private http: HttpClient,
+    public linkElement: ElementRef
   ) {
     this.staticListing = http.get('/assets/TestStaticPayload.json');
+  }
+
+  ngAfterViewInit() {
+    this.childComponents.changes.subscribe((list) => {
+      list.forEach(rippleElement => {
+        const rippleInstance = new MDCRipple(rippleElement.nativeElement);
+      });
+    });
   }
 }
